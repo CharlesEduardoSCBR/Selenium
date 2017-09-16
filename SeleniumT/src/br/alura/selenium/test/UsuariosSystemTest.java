9e43a5de6fc491ac07eda2268be26d084da09307
CharlesEduardoSCBR <charles_eduardo_@hotmail.com>
2017-09-16 13:51:26 -0300

    2.05 - Testando usuario sem nome

--- SeleniumT/src/br/alura/selenium/test/UsuariosSystemTest.java
+++ SeleniumT/src/br/alura/selenium/test/UsuariosSystemTest.java
@@ -2,6 +2,7 @@ package br.alura.selenium.test;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,35 +10,66 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class UsuariosSystemTest {
-	
+
 	static String nomeUsuario = "Adriano Xavier";
 	static String emailUsuario = "axavier@emresa.com.br";
+	private WebDriver driver;
+
+	@Before
+	public void init() {
+	}
+
+	@Before
+	public void endTest() {
+	}
 
 	@Test
 	public void deveAdicionarUmUsuario() {
-		System.setProperty("webdriver.chrome.driver","C:/Users/charl_000/Documents/Cursos/Alura/Trilhas/00 - Java All/00 - Selenium/00-selenium/chromedriver_win32/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		
+		System.setProperty("webdriver.chrome.driver",
+				"C:/Users/charl_000/Documents/Cursos/Alura/Trilhas/00 - Java All/00 - Selenium/00-selenium/chromedriver_win32/chromedriver.exe");
+		driver = new ChromeDriver();
 		driver.get("http://localhost:8080/usuarios/new");
-		
+
 		WebElement nome = driver.findElement(By.name("usuario.nome"));
 		WebElement email = driver.findElement(By.name("usuario.email"));
-		
+
 		nome.sendKeys(nomeUsuario);
 		email.sendKeys(emailUsuario);
-		
+
 		WebElement botaoSalvar = driver.findElement(By.id("btnSalvar"));
 		botaoSalvar.click();
-		
+
 		boolean achouNome = driver.getPageSource().contains(nomeUsuario);
 		boolean achouEmail = driver.getPageSource().contains(emailUsuario);
-		
+
 		assertTrue(achouNome);
 		assertTrue(achouEmail);
 		
 		driver.close();
-		
 	}
-	
 
+	@Test
+	public void deveAdicionarUmUsuarioSemNome() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:/Users/charl_000/Documents/Cursos/Alura/Trilhas/00 - Java All/00 - Selenium/00-selenium/chromedriver_win32/chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		String userEmpty = "";
+		driver.get("http://localhost:8080/usuarios/new");
+
+		WebElement nome = driver.findElement(By.name("usuario.nome"));
+		WebElement email = driver.findElement(By.name("usuario.email"));
+
+		nome.sendKeys(userEmpty);
+		email.sendKeys(emailUsuario);
+
+		WebElement botaoSalvar = driver.findElement(By.id("btnSalvar"));
+		botaoSalvar.click();
+
+		boolean achouErro = driver.getPageSource().contains("Nome obrigatorio!");
+
+		assertTrue(achouErro);
+		
+		driver.close();
+	}
 }
